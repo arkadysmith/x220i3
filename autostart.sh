6b1dfaf32e1bfa26a5706cd0f260512789568046
arkadysmith <nw2025@protonmail.com>
2025-06-18 21:54:12 +0000

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+
+# Autostart applications
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
+# numlockx on &
+dunst &
+# blueman-applet &
+
+# sxhkd
+sxhkd -c ~/.config/i3/sxhkdrc &
